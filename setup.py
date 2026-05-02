a40e615eb956f8d636ffda0139517d175eab90c3
Jonathon Chew <jonchew626@hotmail.com>
2026-05-02 18:56:44 +0100

    fix: where it looks for packages

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 
 setup (
     name="pyflags",
-    version="v0.6.3",
+    version="0.6.3",
     description="`py-flags` is a small Python project for defining and parsing command-line flags.",
     author="Jonathon Chew",
     author_email="jonchew626@hotmail.com",
     url="https://github.com/jonathon-chew/py-flags",
+    package_dir={"": "src"},
     packages=find_packages(where="src", exclude=("tests*",)),
 )
