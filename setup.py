0896e664224fabc0ddf60e62f400a2ab2ab2fb30
Jonathon Chew <jonchew626@hotmail.com>
2026-05-02 11:36:11 +0100

    new: setup.py for installation purposes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup (
+    name="pyflags",
+    version="v0.6.3",
+    description="`py-flags` is a small Python project for defining and parsing command-line flags.",
+    author="Jonathon Chew",
+    author_email="jonchew626@hotmail.com",
+    url="https://github.com/jonathon-chew/py-flags",
+    packages=find_packages(exclude=("tests*")),
+)
